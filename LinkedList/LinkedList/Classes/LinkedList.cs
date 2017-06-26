72e7c0cfb6bf68805f27749586be071274ff46ae
Luis Almanzar <ljalmanzar@gmail.com>
2017-06-25 22:31:25 -0700

    finished insert, remove, gotoprior, print, getcursor, getsize, isfull, isempty

--- LinkedList/LinkedList/Classes/LinkedList.cs
+++ LinkedList/LinkedList/Classes/LinkedList.cs
@@ -1,54 +1,167 @@
 ﻿using System;
-namespace LinkedList.Classes
+namespace LinkedList
 {
-    public class LinkedList<DataType>
-    {
-        private LinkedListNode<DataType> head;
-        private LinkedListNode<DataType> cursor; 
+	public class LinkedList<DataType>
+	{
+        private LinkedListNode<DataType> _head;
+        private LinkedListNode<DataType> _cursor;
+        private int _size;
 
-        public LinkedList()
+		public LinkedList()
+		{
+			_head = _cursor = null;
+            _size = 0;
+		}
+
+		public bool Insert(DataType newItem)
+		{
+			//if cursor & head is null, create new node with data
+			if (_head == null && _cursor == null)
+			{
+                _head = new LinkedListNode<DataType>(newItem, null);
+                _cursor = _head;
+
+                return true;
+			}
+
+            //if cursor is at end of list
+            if (_cursor.Next == null)
+            {
+                _cursor.Next = new LinkedListNode<DataType>(newItem, null);
+                _cursor = _cursor.Next;
+
+                return true;
+            }
+            //if cursor is in the middle
+            else
+            {
+                LinkedListNode<DataType> temp;
+
+                temp = _cursor.Next;
+                _cursor.Next = new LinkedListNode<DataType>(newItem, null);
+                _cursor = _cursor.Next;
+                _cursor.Next = temp;
+
+                return true;
+            }
+		}
+
+		public bool Remove()
+		{
+			//if cursor & head is null, create new node with data
+			if (_head == null && _cursor == null)
+			{
+                return false;
+			}
+
+			//if cursor is at end of list
+			if (_cursor.Next == null)
+			{
+                GoToPrior();
+                _cursor.Next = null;
+                return true;
+			}
+			//if cursor is in the middle
+			else
+			{
+                LinkedListNode<DataType> temp = _cursor.Next;
+                GoToPrior();
+                _cursor.Next = temp;
+				return true;
+			}
+		}
+
+		public bool Replace(DataType newItem)
+		{
+			return true;
+		}
+
+		public bool Clear()
+		{
+			return true;
+		}
+
+		public bool IsEmpty()
+		{
+            return (_head == null ? true : false);
+		}
+
+		public bool IsFull()
+		{
+			return false;
+		}
+
+        public int GetSize()
         {
-            head = cursor = null;
+            return _size;
         }
 
-        public bool Insert(DataType newItem)
+		public bool GoToBeginning()
+		{
+			return true;
+		}
+
+		public bool GoToEnd()
+		{
+			return true;
+		}
+
+		public bool GoToNext()
+		{
+			return true;
+		}
+
+		public bool GoToPrior()
+		{
+            if (_cursor != _head)
+            {
+                LinkedListNode<DataType> temp = _head;
+                while(temp.Next != _cursor)
+                {
+                    temp = temp.Next;
+                }
+
+                _cursor = temp;
+            }
+
+			return true;
+		}
+
+		public DataType GetCursor()
+		{
+            return _cursor.Data;
+		}
+
+		public bool MoveToBeginning()
+		{
+			return true;
+		}
+
+		public bool InsertBefore(DataType newDataItem)
+		{
+			return true;
+		}
+
+        public void Print()
         {
-            return true;
+            Console.Write("List: ");
+
+            LinkedListNode<DataType> printCursor;
+
+            printCursor = _head;
+
+            if (_head != null)
+            {
+                Console.Write("{0} ", printCursor.Data);
+				while (printCursor.Next != null)
+				{
+                    printCursor = printCursor.Next;
+					Console.Write("{0} ", printCursor.Data);
+				}
+            }    
+
+            Console.WriteLine();
         }
 
-        public bool Remove()
-        {
-            return true;
-        }
-
-        public bool Replace(DataType newItem)
-        {
-            return true;
-        }
-
-        public bool Clear()
-        {
-            return true;
-        }
-		/*bool insert(const DataType& newDataItem);
-    bool remove();
-		bool replace(const DataType& newDataItem);
-    bool clear();
-
-		bool isEmpty() const;
-		bool isFull() const;
-
-		bool gotoBeginning();
-		bool gotoEnd();
-		bool gotoNext();
-		bool gotoPrior();
-
-		DataType getCursor() const;
-
-		// Programming exercise 2
-		bool moveToBeginning();
-
-		// Programming exercise 3
-		bool insertBefore(const DataType& newDataItem);*/
-    }
-}
+	}//end of class
+}//end of namespace
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -2,107 +2,6 @@
 
 namespace LinkedList
 {
-	public class LinkedListNode<DataType>
-	{
-		private DataType data;
-		private LinkedListNode<DataType> next;
-
-		public LinkedListNode(DataType data, LinkedListNode<DataType> next)
-		{
-			this.data = data;
-			this.next = next;
-		}
-
-		public DataType Data
-		{
-			get { return this.data; }
-			set { data = value; }
-		}
-
-		public LinkedListNode<DataType> Next
-		{
-			get { return this.next; }
-			set { next = value; }
-		}
-	}
-
-	public class LinkedList<DataType>
-	{
-		private LinkedListNode<DataType> head;
-		private LinkedListNode<DataType> cursor;
-
-		public LinkedList()
-		{
-			head = cursor = null;
-		}
-
-		public bool Insert(DataType newItem)
-		{
-			return true;
-		}
-
-		public bool Remove()
-		{
-			return true;
-		}
-
-		public bool Replace(DataType newItem)
-		{
-			return true;
-		}
-
-		public bool Clear()
-		{
-			return true;
-		}
-
-		public bool IsEmpty()
-		{
-			return true;
-		}
-
-		public bool IsFull()
-		{
-			return false;
-		}
-
-		public bool GoToBeginning()
-		{
-			return true;
-		}
-
-		public bool GoToEnd()
-		{
-			return true;
-		}
-
-		public bool GoToNext()
-		{
-			return true;
-		}
-
-		public bool GoToPrior()
-		{
-            return true;
-		}
-
-		public DataType GetCursor()
-		{
-            DataType temp = head.Data;
-            return temp;
-		}
-
-		public bool MoveToBeginning()
-		{
-			return true;
-		}
-
-		public bool InsertBefore(DataType newDataItem)
-		{
-			return true;
-		}
-	}
-
     class MainClass
     {
         public static void Main(string[] args)
@@ -111,7 +10,38 @@ namespace LinkedList
 
             LinkedList<int> List1 = new LinkedList<int>();
 
+            Console.WriteLine("The list is empty: {0}", List1.IsEmpty());
 
+            List1.Insert(6);
+            List1.Print();
+
+			List1.Insert(169);
+			List1.Print();
+			List1.Insert(70);
+			List1.Print();
+			List1.Insert(45);
+			List1.Print();
+			List1.Insert(20);
+			List1.Print();
+
+            List1.GoToPrior();
+            Console.WriteLine("Cursor at {0}.", List1.GetCursor());
+
+            List1.Insert(96);
+            List1.Print();
+
+			List1.GoToPrior();
+			Console.WriteLine("Cursor at {0}.", List1.GetCursor());
+			List1.GoToPrior();
+			Console.WriteLine("Cursor at {0}.", List1.GetCursor());
+			List1.GoToPrior();
+			Console.WriteLine("Cursor at {0}.", List1.GetCursor());
+			List1.GoToPrior();
+			Console.WriteLine("Cursor at {0}.", List1.GetCursor());
+			List1.GoToPrior();
+			Console.WriteLine("Cursor at {0}.", List1.GetCursor());
+
+            Console.WriteLine("The list is empty: {0}", List1.IsEmpty());
         }
     }
 }
